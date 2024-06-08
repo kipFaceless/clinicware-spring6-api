b1bac0615d011a537bd2588e15765cf151297778
kipFaceless <kipfaceless@gmail.com>
2024-06-08 13:36:41 +0100

    database configuration

---
+++ src/main/java/com/clinicware/clinicware/ClinicwareApplication.java
@@ -0,0 +1,13 @@
+package com.clinicware.clinicware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicwareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clinicware/clinicware/ClinicwareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinicware.clinicware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
